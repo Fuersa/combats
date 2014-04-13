a82d60537326e98c788a803a65744a189adfad17
savka pavel <savka.pavel@outlook.com>
2014-04-13 13:54:21 +0200

    Neede changes

--- src/servlets/WelcomeServlet.java
+++ src/servlets/WelcomeServlet.java
@@ -4,26 +4,20 @@ import java.io.IOException;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 
-/**
- * Servlet implementation class WelcomeServlet
- */
-@WebServlet("/welcome")
 public class WelcomeServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	private static final String VIEW = "/WEB-INF/JSP/welcome.jsp";
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 	throws ServletException, IOException {
+		
+		System.out.println("Welcome servlet");
 		RequestDispatcher dispatcher = request.getRequestDispatcher(VIEW);
 		dispatcher.forward(request, response);
 	}
